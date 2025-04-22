33e4bd51f391f88ae5b0d9c505ecafeabb0ee9d7
khushiram saini <prabhakarsaini800@gmail.com>
2025-04-23 00:14:33 +0530

    Enhance cookie security settings in login response

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -66,8 +66,9 @@ const Login = async (req, res) => {
       // add the token to cookie and send the response back to the user
       res.cookie("token", token, {
         httpOnly: true,
-        // sameSite: "strict",
         secure: true,
+        sameSite: "None",
+
         expires: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
       });
       return res.json(user);
